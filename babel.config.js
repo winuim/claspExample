f2a1a3805f7664d67acd93c4311c558367358504
Uema Yohei <uema.yohei@outlook.jp>
2023-01-23 23:28:35 +0900

    modify appsscript.json, babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,19 +2,19 @@ module.exports = {
   presets: [
     [
       // ES features necessary for user's Node version
-      require('@babel/preset-env').default,
+      '@babel/preset-env',
       {
         targets: {
           node: 'current',
         },
       },
     ],
-    [require('@babel/preset-typescript').default],
+    ['@babel/preset-typescript'],
   ],
   plugins: [
     // Polyfills the runtime needed for async/await, generators, and friends
     // https://babeljs.io/docs/en/babel-plugin-transform-runtime
-    [require('@babel/plugin-transform-runtime').default],
+    ['@babel/plugin-transform-runtime'],
     // https://babeljs.io/docs/en/babel-plugin-proposal-decorators
     ['@babel/plugin-proposal-decorators', {version: 'legacy'}],
     '@babel/plugin-proposal-class-properties',
