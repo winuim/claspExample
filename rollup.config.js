1f18db9960a2abfd4b09f7c09daeee6a91003e0f
Uema Yohei <uema.yohei@outlook.jp>
2023-01-26 23:53:49 +0900

    dayjsサンプル カスタムメニュー、ダイアログ、サイドメニューのサンプル

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import {babel} from '@rollup/plugin-babel';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
 
 const extensions = ['.ts', '.js'];
@@ -30,6 +31,7 @@ export default {
       mainFields: ['jsnext:main', 'module', 'main'],
     }),
     babel({extensions, babelHelpers: 'runtime'}),
+    commonjs(),
     copy({
       targets: [
         {src: 'appsscript.json', dest: 'dist/'},
